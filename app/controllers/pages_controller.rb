af2be0470d7b62f6934fce1640aa4efe5dd777ab
Louise Darche <louise.darche@gmail.com>
2019-03-06 18:35:08 +0100

    try to add answers to csv

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,7 @@ class PagesController < ApplicationController
   def answer
     @answer = Answer.new(answer_params)
     if @answer.save
+      save_to_csv
       redirect_to root_path
     else
       @notification = "🙌 Votre réponse a bien été prise en compte! 🙌"
@@ -30,6 +31,20 @@ class PagesController < ApplicationController
     params.require(:answer).permit(:name, :email, :answer_dinner, :nb_dinner, :answer_brunch, :nb_brunch)
   end
 
+  def save_to_csv
+    csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+    filepath    = File.join(__dir__, '../../db/answers.csv')
+
+    CSV.open(filepath, 'wb', csv_options) do |csv|
+      csv << ['Name', 'Email', 'answer_dinner', 'nb_dinner', 'answer_brunch', 'nb_brunch']
+      Answer.all.each do |answer|
+        csv << answer
+      end
+      # ...
+    end
+
+
+  end
   def load_csv
     filepath = File.join(__dir__, '../../db/emails.csv')
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
