f7de5a6481543f18bb21f661dcd0c47799500715
Louise Darche <louise.darche@gmail.com>
2019-03-21 19:36:03 +0100

    added sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,12 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 initMapbox();
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Merci pour votre réponse !",
+  text: "Votre réponse a bien été prise en compte 😃",
+  icon: "success"
+});
